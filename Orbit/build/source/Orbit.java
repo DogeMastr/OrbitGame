4823d9fe7fb1ea13326fb70ecf25090c95f0f981
Doge <mastrdoge@gmail.com>
2019-03-13 17:12:01 +0000

    Atom builds

--- Orbit/build/source/Orbit.java
+++ Orbit/build/source/Orbit.java
@@ -130,6 +130,7 @@ class meteor {
   float x;
   float y;
   float radius;
+  float pos;
 
   float speed;
   float rotation;
@@ -137,11 +138,12 @@ class meteor {
   float xMoveSpeed;
   float yMoveSpeed;
 
-  // meteor(random(20,50),random(20,50),random(10,20),random(5,10),random(0,PI*2))
-  meteor(float _x, float _y, float _r, float _s, float _ro) {
+  // meteor(random(int)(1,4),random(10,20),random(5,10));
+  meteor(float _pos, float _r, float _s) {
     x = _x;
     y = _y;
     radius = _r;
+    pos = _pos;
 
     speed = _s;
     rotation = _ro;
@@ -159,22 +161,66 @@ class meteor {
     //       2*PI
     // PI*1.5    PI/2
     //       PI
-    if (rotation >= 0 && rotation <= PI/2) {
-      //going top right
-      x = 0 - x;
-      y = 0 - y;
-    } else if (rotation >= PI/2 && rotation <= PI) {
-      //going bottom right
-      x = 0 - x;
-      y = y + height;
-    } else if (rotation >= PI && rotation <= PI*1.5f) {
-      //going bottom left
-      x = x + width;
-      y = y + height;
-    } else if (rotation >= PI*1.5f && rotation <= PI*2) {
-      //going top left
-      x = x + width;
-      y = 0 - y;
+
+    // //replace these with randoms thanks
+    // if (rotation >= 0 && rotation <= PI/2) {
+    //   //going top right
+    //   x = 0 - x;
+    //   y = 0 - y;
+    // } else if (rotation >= PI/2 && rotation <= PI) {
+    //   //going bottom right
+    //   x = 0 - x;
+    //   y = y + height;
+    // } else if (rotation >= PI && rotation <= PI*1.5) {
+    //   //going bottom left
+    //   x = x + width;
+    //   y = y + height;
+    // } else if (rotation >= PI*1.5 && rotation <= PI*2) {
+    //   //going top left
+    //   x = x + width;
+    //   y = 0 - y;
+    // }
+
+    /*
+    change everything to this pleasseee
+
+    int position = random(1,4); whatever im tires now
+
+    if pos 1 - bottom half of the screen, rotation is random(PI*1.5, PI/2);
+    if pos 2 - top half rotation is random(PI/2, PI*1.5);
+    if pos 3 - left half rotation is random(0,PI);
+    if pos 4 - right half rotation is random(PI,PI*2);
+
+    also do random for x & y values that make sence thanks ttyl
+    pos 1 - y is width + 60 x is random(-60, width + 60);
+    pos 2 - y is - 60 x is the same as last time
+    pos 3 swap the x and y in pos 1 bit hieght
+    pos 4 wowie
+    */
+
+    if(pos == 1){
+      rotation = random(PI*1.5f, PI/2);
+
+      x = random(-60, width + 60);
+      y = width + 60;
+
+    } else if(pos == 2){
+      rotation = random(PI/2, PI*1.5f);
+
+      x = random(-60, width + 60);
+      y = -60;
+
+    } else if(pos == 3){
+      rotation = random(0,PI);
+
+      x = width + 60;
+      y = random(-60, height + 60);
+      
+    } else {
+      rotation = random(PI, PI/2);
+
+      x = -60;
+      y = random(-60, height + 60);;
     }
   }
 
