7cac782bd559e079e2653003033c327260015464
Doge <mastrdoge@gmail.com>
2019-03-16 11:09:25 +0000

    fixed everything but i didnt

--- Orbit/build/source/Orbit.java
+++ Orbit/build/source/Orbit.java
@@ -125,7 +125,7 @@ class blackhole {
     ellipse(x, y, radius*2, radius*2);
   }
 }
-class meteor {
+	class meteor {
 
   float x;
   float y;
@@ -150,71 +150,30 @@ class meteor {
 
     println("init: "+ this);
     println(pos);
+		println(rotation);
   }
 
   public void initRotation() {
-    //x & y values are random 20 - 50, depending on rotation add width +| height
-    //Clock face but in PI
-    //       PI*2
-    // PI*1.5    PI/2
-    //       PI
-
-    // //replace these with randoms thanks
-    // if (rotation >= 0 && rotation <= PI/2) {
-    //   //going top right
-    //   x = 0 - x;
-    //   y = 0 - y;
-    // } else if (rotation >= PI/2 && rotation <= PI) {
-    //   //going bottom right
-    //   x = 0 - x;
-    //   y = y + height;
-    // } else if (rotation >= PI && rotation <= PI*1.5) {
-    //   //going bottom left
-    //   x = x + width;
-    //   y = y + height;
-    // } else if (rotation >= PI*1.5 && rotation <= PI*2) {
-    //   //going top left
-    //   x = x + width;
-    //   y = 0 - y;
-    // }
-
-    /*
-    change everything to this pleasseee
-
-    int position = random(1,4); whatever im tires now
-
-    if pos 1 - bottom half of the screen, rotation is random(PI*1.5, PI/2);
-    if pos 2 - top half rotation is random(PI/2, PI*1.5);
-    if pos 3 - left half rotation is random(0,PI);
-    if pos 4 - right half rotation is random(PI,PI*2);
-
-    also do random for x & y values that make sence thanks ttyl
-    pos 1 - y is width + 60 x is random(-60, width + 60);
-    pos 2 - y is - 60 x is the same as last time
-    pos 3 swap the x and y in pos 1 bit hieght
-    pos 4 wowie
-    */
-
     if(pos == 1){
-      rotation = random(PI*1.5f,PI*2.5f);
+			rotation = random(0.523599f,2.094395f);
 
 			x = -60;
-			y = random(-60, height + 60);;
+			y = random(-60, height + 60);
 
     } else if(pos == 2){
-      rotation = random(PI/2, PI*1.5f);
+      rotation = random(2.094395f,3.665191f);
 
 			x = width + 60;
 			y = random(-60, height + 60);
 
     } else if(pos == 3){
-      rotation = random(0,PI);
+			rotation = random(3.665191f,5.235988f);
 
 			x = random(-60, width + 60);
-			y = width + 60;
+				y = width + 60;
 
     } else {
-      rotation = random(PI, PI/2);
+      rotation = random(5.235988f,6.806784f);
 
 			x = random(-60, width + 60);
 			y = -60;
